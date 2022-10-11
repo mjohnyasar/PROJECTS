6719e73243b091bf50bbdf8ff282592a6ebe11e2
M. John Yasar <m.john.yasar@gmail.com>
2022-10-11 11:25:07 -0700

    Add files via upload

---
+++ portfolio_demo.py
@@ -0,0 +1,14 @@
+# import numpy as np
+# import pandas as pd
+import streamlit as st
+# import seaborn as sns
+#import matplotlib.pyplot as plt
+
+
+st.header(" M John Yasar PORTFOLIO Page")
+st.image("/content/mjohnyasar.jpg",width=150,caption="mjohnyasar ")
+
+st.subheader("Customer RFM ")
+st.write("Check Out This: [Customer RFM Analysis](https://mjohnyasar-internship-rfm-streamlit-rfm-hlyqxv.streamlitapp.com/)")
+st.info("In this project we first calculate RFM metrics (Recency, Frequency and Monetary ) for each customer and then make alanaysis on them. ")
+st.warning("What is  RFM: RFM is a method used for analyzing customer value. It is commonly used in database marketing and direct marketing and has received particular attention in retail and professional services industries. RFM stands for the three dimensions: Recency – How recently did the customer purchase? ")
