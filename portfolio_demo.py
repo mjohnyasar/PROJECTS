a7458d296bc9784837c3cbf76c89256b9ab38355
M. John Yasar <m.john.yasar@gmail.com>
2022-10-11 17:56:16 -0700

    Update portfolio_demo.py

--- portfolio_demo.py
+++ portfolio_demo.py
@@ -10,8 +10,10 @@ st.image("mjohnyasar.jpg",width=150,caption="mjohnyasar ")
 
 st.subheader("Sales Income")
 st.write("Check Out This: [Sales Income Estimation](https://mjohnyasa-total-sales-revenue-preditiontotal-sales-demo-7zb8rq.streamlitapp.com/)")
-st.info("SARIMAX model were used to predict  daily, weekly and monthly total sales revenue ")
+st.info("SARIMAX model were used to predict next days, weeks and months sales revenue ")
 st.warning("SARIMAX(Seasonal Auto-Regressive Integrated Moving Average with eXogenous factors) is an updated version of the ARIMA model. ARIMA includes an autoregressive integrated moving average, while SARIMAX includes seasonal effects and eXogenous factors with the autoregressive and moving average component in the model.")
+
+
 st.subheader("Customer RFM ")
 st.write("Check Out This: [Customer RFM Analysis](https://mjohnyasar-internship-rfm-streamlit-rfm-hlyqxv.streamlitapp.com/)")
 st.info("In this project   first of all for each customer  RFM metrics were calculated (Recency, Frequency and Monetary ")
