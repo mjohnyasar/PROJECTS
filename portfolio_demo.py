05883df7bfc7ebffdfa3493f14f4fd7426ab8499
M. John Yasar <m.john.yasar@gmail.com>
2022-10-12 07:48:46 -0700

    Update portfolio_demo.py

--- portfolio_demo.py
+++ portfolio_demo.py
@@ -4,7 +4,7 @@ import streamlit as st
 # import seaborn as sns
 #import matplotlib.pyplot as plt
 
-st.header(" M John Yasar PORTFOLIO Link Page")
+st.header(" M JOHN YASAR PORTFOLIO LINK PAGE")
 st.image("mjohnyasar.jpg",width=150,caption="mjohnyasar ")
 
 st.header("Social accounts")
@@ -14,13 +14,13 @@ st.write("Kaggle: [https://www.kaggle.com/..](https://www.kaggle.com/muharremyas
 st.write("Stackoverflow: [https://stackoverflow.com/..](https://stackoverflow.com/users/16801544/m-john)")
 
 st.header("Some deployment and project links")
-st.subheader("Time Series Prediction, ARIMA, Sarimax")
+st.subheader("©  Time Series Prediction, ARIMA, Sarimax")
 st.write("Check Out This: [Sales Income Estimation](https://mjohnyasa-total-sales-revenue-preditiontotal-sales-demo-7zb8rq.streamlitapp.com/)")
 st.info("SARIMAX model were used to predict next days, weeks and months sales revenue ")
 st.warning("SARIMAX (Seasonal Auto-Regressive Integrated Moving Average with eXogenous factors) is an updated version of the ARIMA model. ARIMA includes an autoregressive integrated moving average, while SARIMAX includes seasonal effects and eXogenous factors with the autoregressive and moving average component in the model.")
 
 
-st.subheader("Customer RFM, segmentation, loyalty, lifetime value, churn")
+st.subheader("©  Customer RFM, segmentation, loyalty, lifetime value, churn")
 st.write("Check Out This: [Customer RFM Analysis](https://mjohnyasar-internship-rfm-streamlit-rfm-hlyqxv.streamlitapp.com/)")
 st.info("In this project   first of all for each customer  RFM metrics were calculated (Recency, Frequency and Monetary ")
 st.warning("RFM is a method used for analyzing customer value. It is commonly used in database marketing and direct marketing and has received particular attention in retail and professional services industries. RFM stands for the three dimensions: Recency – How recently did the customer purchase? ")
