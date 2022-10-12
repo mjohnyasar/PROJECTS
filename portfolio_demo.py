8ec934ec7871e5e199c8c6e0065908743db05ad2
M. John Yasar <m.john.yasar@gmail.com>
2022-10-11 20:47:27 -0700

    Update portfolio_demo.py

--- portfolio_demo.py
+++ portfolio_demo.py
@@ -17,4 +17,4 @@ st.warning("SARIMAX (Seasonal Auto-Regressive Integrated Moving Average with eXo
 st.subheader("Customer RFM, segmentation, loyalty, lifetime value, churn")
 st.write("Check Out This: [Customer RFM Analysis](https://mjohnyasar-internship-rfm-streamlit-rfm-hlyqxv.streamlitapp.com/)")
 st.info("In this project   first of all for each customer  RFM metrics were calculated (Recency, Frequency and Monetary ")
-st.warning("What is  RFM: RFM is a method used for analyzing customer value. It is commonly used in database marketing and direct marketing and has received particular attention in retail and professional services industries. RFM stands for the three dimensions: Recency – How recently did the customer purchase? ")
+st.warning("RFM is a method used for analyzing customer value. It is commonly used in database marketing and direct marketing and has received particular attention in retail and professional services industries. RFM stands for the three dimensions: Recency – How recently did the customer purchase? ")
