2957cca2e43487526672b0fe428704c74ae6f7d2
M. John Yasar <m.john.yasar@gmail.com>
2022-10-11 17:42:53 -0700

    Update portfolio_demo.py

--- portfolio_demo.py
+++ portfolio_demo.py
@@ -10,15 +10,10 @@ st.image("mjohnyasar.jpg",width=150,caption="mjohnyasar ")
 
 st.subheader("Sales Income")
 st.write("Check Out This: [Sales Income Estimation](https://mjohnyasa-total-sales-revenue-preditiontotal-sales-demo-7zb8rq.streamlitapp.com/)")
-st.info("In this project we first calculate RFM metrics (Recency, Frequency and Monetary ) for each customer and then make alanaysis on them. ")
+st.info("In this project   first of all for each customer  RFM metrics were calculated (Recency, Frequency and Monetary ) ")
 st.warning("What is  RFM: RFM is a method used for analyzing customer value. It is commonly used in database marketing and direct marketing and has received particular attention in retail and professional services industries. RFM stands for the three dimensions: Recency – How recently did the customer purchase? ")
-
-  
- 
-st.header(" M John Yasar PORTFOLIO Page")
-st.image("mjohnyasar.jpg",width=150,caption="mjohnyasar ")
 
 st.subheader("Customer RFM ")
 st.write("Check Out This: [Customer RFM Analysis](https://mjohnyasar-internship-rfm-streamlit-rfm-hlyqxv.streamlitapp.com/)")
-st.info("In this project we first calculate RFM metrics (Recency, Frequency and Monetary ) for each customer and then make alanaysis on them. ")
+st.info("In this project   first of all for each customer  RFM metrics were calculated (Recency, Frequency and Monetary ")
 st.warning("What is  RFM: RFM is a method used for analyzing customer value. It is commonly used in database marketing and direct marketing and has received particular attention in retail and professional services industries. RFM stands for the three dimensions: Recency – How recently did the customer purchase? ")
