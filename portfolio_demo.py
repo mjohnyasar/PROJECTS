04b0958ce1c30bec2fd584f7eaa3cfea909e876c
M. John Yasar <m.john.yasar@gmail.com>
2022-10-11 17:51:30 -0700

    Update portfolio_demo.py

--- portfolio_demo.py
+++ portfolio_demo.py
@@ -10,9 +10,8 @@ st.image("mjohnyasar.jpg",width=150,caption="mjohnyasar ")
 
 st.subheader("Sales Income")
 st.write("Check Out This: [Sales Income Estimation](https://mjohnyasa-total-sales-revenue-preditiontotal-sales-demo-7zb8rq.streamlitapp.com/)")
-st.info("In this project   first of all for each customer  RFM metrics were calculated (Recency, Frequency and Monetary ) ")
-st.warning("What is  RFM: RFM is a method used for analyzing customer value. It is commonly used in database marketing and direct marketing and has received particular attention in retail and professional services industries. RFM stands for the three dimensions: Recency – How recently did the customer purchase? ")
-
+st.info("SARIMAX model were used to predict  daily, weekly and monthly total sales revenue ")
+st.warning("SARIMAX(Seasonal Auto-Regressive Integrated Moving Average with eXogenous factors) is an updated version of the ARIMA model. ARIMA includes an autoregressive integrated moving average, while SARIMAX includes seasonal effects and eXogenous factors with the autoregressive and moving average component in the model.")
 st.subheader("Customer RFM ")
 st.write("Check Out This: [Customer RFM Analysis](https://mjohnyasar-internship-rfm-streamlit-rfm-hlyqxv.streamlitapp.com/)")
 st.info("In this project   first of all for each customer  RFM metrics were calculated (Recency, Frequency and Monetary ")
