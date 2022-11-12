98c556111d50ffe896ed82bc2fc32abe14027d5d
M. John Yasar <m.john.yasar@gmail.com>
2022-11-12 15:41:19 -0800

    Update portfolio_demo.py

--- portfolio_demo.py
+++ portfolio_demo.py
@@ -19,15 +19,12 @@ st.write("Check Out This: [Sales Income Estimation](https://mjohnyasa-total-sale
 st.info("SARIMAX model were used to predict next days, weeks and months sales revenue ")
 st.warning("SARIMAX (Seasonal Auto-Regressive Integrated Moving Average with eXogenous factors) is an updated version of the ARIMA model. ARIMA includes an autoregressive integrated moving average, while SARIMAX includes seasonal effects and eXogenous factors with the autoregressive and moving average component in the model.")
 
-
 st.subheader("©    Customer RFM, segmentation, loyalty, lifetime value, churn")
 st.write("Check Out This: [Customer RFM Analysis](https://mjohnyasar-internship-rfm-streamlit-rfm-hlyqxv.streamlitapp.com/)")
 st.info("In this project   first of all for each customer  RFM metrics were calculated (Recency, Frequency and Monetary ")
 st.warning("RFM is a method used for analyzing customer value. It is commonly used in database marketing and direct marketing and has received particular attention in retail and professional services industries. RFM stands for the three dimensions: Recency – How recently did the customer purchase? ")
 
-
-
 st.subheader("©    Power BI project samples")
 st.write("Check Out This: [Power BI Reports](https://drive.google.com/file/d/1bjh_J2ZsPv22RyVWsKoXoL0HTX-dxCh-/view?usp=sharing)")
 st.info("In these projects almost all aspects of PowerBI capabilities are shown ")
-st.warning("Here some report samples are presented  for the company's  Executives and HR Managements)
+st.warning("Here some report samples are presented  for the company's  Executives and HR Managements")
